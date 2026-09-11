c56f6ad6934549066c78d68e8c1f09cc63a06469
anush126 <anushsubedar007@gmail.com>
2026-09-11 19:43:41 +0530

    Time: 1 ms (95.88%), Space: 42.8 MB (88.84%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,15 +1,30 @@
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         String res = "";
+//         int l = word1.length() + word2.length();
+//         for(int i = 0; i < l; i++){
+//             if(i < word1.length()){
+//                 res += word1.charAt(i);
+//             }
+//             if(i < word2.length()){
+//                 res += word2.charAt(i);
+//             }
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String res = "";
-        int l = word1.length() + word2.length();
-        for(int i = 0; i < l; i++){
-            if(i < word1.length()){
-                res += word1.charAt(i);
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0;i<Math.max(word1.length() , word2.length());i++){
+            if(i<word1.length()){
+                ans.append(word1.charAt(i));
             }
-            if(i < word2.length()){
-                res += word2.charAt(i);
+            if(i<word2.length()){
+                ans.append(word2.charAt(i));
             }
         }
-        return res;
+        return ans.toString();
     }
 }
