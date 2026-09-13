73db5a50764ac8a9457e5a00fb89df4b57e9b258
anush126 <anushsubedar007@gmail.com>
2026-09-13 19:33:02 +0530

    Time: 3 ms (94.07%), Space: 44.6 MB (56.05%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        int a[] = new int[26];
+        int b[] = new int[26];
+
+        for(char c : s.toCharArray()){
+            a[c - 'a']++;
+        }
+        
+        for(char c : t.toCharArray()){
+            b[c - 'a']++;
+        }
+
+        return Arrays.equals(a,b);
+    }
+}
